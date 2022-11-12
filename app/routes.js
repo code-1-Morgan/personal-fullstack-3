cf9d6ba6cc050e16221349d62304b95c0cad7da3
mdowning96 <morganodowning@gmail.com>
2022-11-11 23:41:12 -0500

    updated

--- app/routes.js
+++ app/routes.js
@@ -29,27 +29,28 @@ module.exports = function(app, passport, db) {
 // message board routes ===============================================================
 
     app.post('/messages', (req, res) => {
-      db.collection('messages').save({root: req.body.root, sign: req.body.sign}, (err, result) => {
+      db.collection('messages').save({root: req.body.root}, (err, result) => {
         if (err) return console.log(err)
         console.log('saved to database')
         res.redirect('/profile')
       })
     })
 
-    // app.put('/thumbUp', (req, res) => {
-    //   db.collection('messages')
-    //   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    //     $set: {
-    //       thumbUp:req.body.thumbUp + 1
-    //     }
-    //   }, {
-    //     sort: {_id: -1},
-    //     upsert: true
-    //   }, (err, result) => {
-    //     if (err) return res.send(err)
-    //     res.send(result)
-    //   })
-    // })
+    app.put('/see', (req, res) => {
+      db.collection('messages')
+      .findOneAndUpdate({root: req.body.root}, {
+        $set: {
+          see: see,
+
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
 
     // app.put('/thumbDown', (req, res) => {
     //   db.collection('messages')
@@ -67,7 +68,7 @@ module.exports = function(app, passport, db) {
     // })
 
     app.delete('/messages', (req, res) => {
-      db.collection('messages').findOneAndDelete({root: req.body.root, sign: req.body.sign}, (err, result) => {
+      db.collection('messages').findOneAndDelete({root: req.body.root}, (err, result) => {
         if (err) return res.send(500, err)
         res.send('Message deleted!')
       })
--- public/main.js
+++ public/main.js
@@ -1,71 +1,66 @@
 //I recieved help from Void, Will, Shawna, Pedro, Vikiana, Sarah, and Ty
-document.querySelector('form').addEventListener('submit', getChordPic)
+// document.querySelector('form').addEventListener('submit', getChordPic)
 var trash = document.getElementsByClassName("fa-trash-o");
+var see = document.getElementsByClassName("fa-eye");
 
-function getChordPic(){
-  const root = document.getElementById("#rootSelect").value;
-  if(root === 'a'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else if(root === 'b'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else if(root === 'c'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else if(root === 'd'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else if(root === 'e'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else if(root === 'f'){
-    document.querySelector('.a').classList.remove('hidden')
-  }else{
-    document.querySelector('.a').classList.remove('hidden')
-  }
-}
+// function getChordPic(){
+//   const root = document.getElementById("#rootSelect").value;
+//   console.log('root select')
+//   if(root === 'a'){
+//     document.querySelector('#a').classList.src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/a-chord-diagram.png'
+//   }else if(root === 'b'){
+//     document.querySelector('.b').classList.remove('d-none')
+//   }else if(root === 'c'){
+//     document.querySelector('.c').classList.remove('d-none')
+//   }else if(root === 'd'){
+//     document.querySelector('.d').classList.remove('d-none')
+//   }else if(root === 'e'){
+//     document.querySelector('.e').classList.remove('d-none')
+//   }else if(root === 'f'){
+//     document.querySelector('.f').classList.remove('d-none')
+//   }else{
+//     document.querySelector('.g').classList.remove('d-none')
+//   }
+// }
 
-// Array.from(thumbUp).forEach(function(element) {
-//       element.addEventListener('click', function(){
-//         const name = this.parentNode.parentNode.childNodes[1].innerText
-//         const msg = this.parentNode.parentNode.childNodes[3].innerText
-//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-//         fetch('thumbUp', {
-//           method: 'put',
-//           headers: {'Content-Type': 'application/json'},
-//           body: JSON.stringify({
-//             'name': name,
-//             'msg': msg,
-//             'thumbUp':thumbUp
-//           })
-//         })
-//         .then(response => {
-//           if (response.ok) return response.json()
-//         })
-//         .then(data => {
-//           console.log(data)
-//           window.location.reload(true)
-//         })
-//       });
-// });
-
-// Array.from(thumbDown).forEach(function(element) {
-//   element.addEventListener('click', function(){
-//     const root = this.parentNode.parentNode.childNodes[1].innerText
-//     const sign = this.parentNode.parentNode.childNodes[3].innerText
-//     fetch('thumbDown', {
-//       method: 'put',
-//       headers: {'Content-Type': 'application/json'},
-//       body: JSON.stringify({
-//         'root': root,
-//         'sign': sign,
-//       })
-//     })
-//     .then(response => {
-//       if (response.ok) return response.json()
-//     })
-//     .then(data => {
-//       console.log(data)
-//       window.location.reload(true)
-//     })
-//   });
-// });
+Array.from(see).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const root = this.parentNode.parentNode.childNodes[1].innerText
+        const see = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        // const image = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+        fetch('see', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'root': root,
+            'see': see,
+            // 'image': image
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          if(root === 'a'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/a-chord-diagram.png'
+          }else if(root === 'b'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/b-chord-4.png'
+          }else if(root === 'c'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/c-chord-1.png.webp'
+          }else if(root === 'd'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/d-chord.png.webp'
+          }else if(root === 'e'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/e-chord.png.webp'
+          }else if(root === 'f'){
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/f-chord-2.png.webp'
+          }else{
+            document.querySelector('img').src = 'https://www.guitarlessons.org/wp-content/uploads/2022/07/g-chord-1.png.webp'
+          }
+          // window.location.reload(true)
+        })
+      });
+});
 
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
